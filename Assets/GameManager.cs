979b64f82953578b4024919173e90a7c14710b77
정한성 <hanseongv@naver.com>
2021-08-28 10:52:51 +0900

    잠시 커밋

---
+++ Assets/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<SpriteRenderer> charactorSkinSprite;
+
+    public List<Sprite> skinSpriteWhite;
+    public List<Sprite> skinSpriteWhite2;
+    public List<Sprite> skinSpriteBlack;
+    public List<Sprite> skinSpriteNormal;
+
+    private int i = 0;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                charactorSkinSprite[i].sprite = skinSpriteWhite[i];
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                charactorSkinSprite[i].sprite = skinSpriteWhite2[i];
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                charactorSkinSprite[i].sprite = skinSpriteBlack[i];
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                charactorSkinSprite[i].sprite = skinSpriteNormal[i];
+            }
+        }
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    if (skinSprite.Count - 2 < i) //0<1
+        //    {
+        //        i = 0;
+        //    }
+        //    else i++;
+        //    skinSprite.sprite = skinSprite[i];
+        //}
+    }
+}
