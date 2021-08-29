1cd4e271f34bf9729273da323a1c815196a025bc
정한성 <hanseongv@naver.com>
2021-08-29 23:12:20 +0900

    유니티 에디터에서도 종료 가능

--- Assets/QuitBtn.cs
+++ Assets/QuitBtn.cs
@@ -6,6 +6,11 @@ public class QuitBtn : MonoBehaviour
 {
     public void OnBtnClick()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+
+        Application.Quit(); // 어플리케이션 종료
+#endif
     }
 }
