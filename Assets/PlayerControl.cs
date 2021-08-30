80d5e281f0db207d0e716a7cc54dd2866b20d63e
정한성 <hanseongv@naver.com>
2021-08-31 00:35:10 +0900

    이동 시 반전

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -9,10 +9,15 @@ public class PlayerControl : MonoBehaviour
     public float playerSpeed = 3;
     public float playerJumpPower = 1;
     private Rigidbody2D rigid2d;
+    private Animator anim;
+    private GameObject gameobject;
 
     private void Start()
     {
         rigid2d = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+        //gameobject = GetComponentInChildren<GameObject>();
+        gameobject = transform.GetChild(0).gameObject;
     }
 
     private void Update()
@@ -25,11 +30,17 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(new Vector3(playerSpeed * Time.deltaTime, 0, 0));
+            anim.SetBool("isMove", true);
+            gameobject.transform.rotation = Quaternion.Euler(0, 180, 0);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(new Vector3(-playerSpeed * Time.deltaTime, 0, 0));
+            anim.SetBool("isMove", true);
+            gameobject.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+        else anim.SetBool("isMove", false);
+
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, playerJumpPower);
