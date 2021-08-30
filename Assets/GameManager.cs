8635ba90bf47b4d5398fb26cd901409fd2bae580
정한성 <hanseongv@naver.com>
2021-08-30 22:37:45 +0900

    플레이어 컨트롤 스크립트 추가, 이동하기 점프하기 추가

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,56 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<SpriteRenderer> charactorSkinSprite;
+    //public List<SpriteRenderer> charactorSkinSprite;
 
-    public List<Sprite> skinSpriteWhite;
-    public List<Sprite> skinSpriteWhite2;
-    public List<Sprite> skinSpriteBlack;
-    public List<Sprite> skinSpriteNormal;
+    //public List<Sprite> skinSpriteWhite;
+    //public List<Sprite> skinSpriteWhite2;
+    //public List<Sprite> skinSpriteBlack;
+    //public List<Sprite> skinSpriteNormal;
 
-    private void Start()
-    {
-    }
+    //private void Start()
+    //{
+    //}
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                charactorSkinSprite[i].sprite = skinSpriteWhite[i];
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                charactorSkinSprite[i].sprite = skinSpriteWhite2[i];
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                charactorSkinSprite[i].sprite = skinSpriteBlack[i];
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                charactorSkinSprite[i].sprite = skinSpriteNormal[i];
-            }
-        }
-        //if (Input.GetKeyDown(KeyCode.Alpha2))
-        //{
-        //    if (skinSprite.Count - 2 < i) //0<1
-        //    {
-        //        i = 0;
-        //    }
-        //    else i++;
-        //    skinSprite.sprite = skinSprite[i];
-        //}
-    }
+    //// Update is called once per frame
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Alpha1))
+    //    {
+    //        for (int i = 0; i < 6; i++)
+    //        {
+    //            charactorSkinSprite[i].sprite = skinSpriteWhite[i];
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha2))
+    //    {
+    //        for (int i = 0; i < 6; i++)
+    //        {
+    //            charactorSkinSprite[i].sprite = skinSpriteWhite2[i];
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha3))
+    //    {
+    //        for (int i = 0; i < 6; i++)
+    //        {
+    //            charactorSkinSprite[i].sprite = skinSpriteBlack[i];
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Alpha4))
+    //    {
+    //        for (int i = 0; i < 6; i++)
+    //        {
+    //            charactorSkinSprite[i].sprite = skinSpriteNormal[i];
+    //        }
+    //    }
+    //    //if (Input.GetKeyDown(KeyCode.Alpha2))
+    //    //{
+    //    //    if (skinSprite.Count - 2 < i) //0<1
+    //    //    {
+    //    //        i = 0;
+    //    //    }
+    //    //    else i++;
+    //    //    skinSprite.sprite = skinSprite[i];
+    //    //}
+    //}
 }
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float move;
+    public float playerSpeed = 3;
+    public float playerJumpPower = 1;
+    private Rigidbody2D rigid2d;
+
+    private void Start()
+    {
+        rigid2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(playerSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-playerSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, playerJumpPower);
+            //transform.Translate(new Vector3(0, playerJumpPower * Time.deltaTime, 0));
+            //rigid2d.AddForce(new Vector2(0, playerJumpPower), ForceMode2D.Impulse);
+        }
+    }
+}
